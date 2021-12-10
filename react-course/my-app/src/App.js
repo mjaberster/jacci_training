7c4907087c4eff8ff2f34f14411f22442b1cb26f
Marwan Jaber <mjaberster@gmail.com>
2021-12-10 12:40:09 +0200

    working with nav bar

--- react-course/my-app/src/App.js
+++ react-course/my-app/src/App.js
@@ -4,16 +4,15 @@ import AgendaList from './components/agenda-list/AgendaList';
 import Home from './components/Home'
 import data from './components/data/agenda.json';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-
-
+import MainNavigation from './components/navigation/MainNavigation';
+import UsersList from './components/users/UsersList';
+import StudentCourseList from './components/course/student-courses';
 const App = () => {
   const [dataState, setAgendaItems] = useState(data);
   const AddNewItem = (item) => {
     
     console.log("onNewItem " + dataState);
-    
-    // setAgendaItems(dataState.concat(item));
-       
+           
     setAgendaItems((prevAgendaItems) => {
       return prevAgendaItems.concat(item);
     });
@@ -21,16 +20,27 @@ const App = () => {
 
 
   return (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/agendalist" element={
-        <div>
-          <AgendaList courseAgendaItems={dataState} onAddItem={AddNewItem}/>
-        </div>
-      } />
-    </Routes>
-  </Router>
+    <Router>
+        <MainNavigation />
+        <main>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/courses" element={
+                <div>
+                  <AgendaList courseAgendaItems={dataState} onAddItem={AddNewItem}/>
+                </div>
+              } />
+              <Route path="/students" element={
+                <div>
+                  <UsersList />
+                </div>
+              } />
+              <Route path="/student/:studentid" element={
+                <StudentCourseList/>
+              } />
+            </Routes>
+        </main>
+      </Router>
   );
 }
 
